7a7259e219b76ec688a65f8562ff535542f0276b
misbahShaheen <46444321+misbahShaheen@users.noreply.github.com>
2019-01-08 19:29:34 +0500

    first on git hub
    form html data handle on php form

---
+++ index.php
@@ -0,0 +1,26 @@
+ <<!DOCTYPE html>
+ <html>
+ <head>
+     <meta charset="utf-8" />
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <title>Page Title</title>
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+     <script src="main.js"></script>
+ </head>
+ <body>
+     
+ <?php
+  echo $_POST["txt-f-name"];
+  echo $_POST["txt-l-name"];
+  echo $_POST["txt-fa-name"];
+  echo $_POST["CNIC"];
+  echo $_POST["phone_no"];
+  echo $_POST["address"];
+  echo $_POST["gender"];
+  echo $_POST["email"];
+  echo $_POST["course"];
+  echo $_POST["txt-area"];
+ ?>
+ </body>
+ </html>
